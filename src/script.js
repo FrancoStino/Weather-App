ebe24f0a81494c6b5b253166bea9c0d060260828
heliaakbari <heliaakbari@aut.ac.ir>
2022-03-17 14:38:48 +0330

    conversion for temp done

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-//update date and hour and week
+let apiKey = "5c295f8a5977862a68c4b22eb14217b5";
+
 function lastUpdated(timestamp){
 let date = new Date(timestamp);
 let updated = document.querySelector("#updated");
@@ -57,38 +58,21 @@ daySmall.innerHTML = weekSmall[(day + 4) % 7];
 daySmall = document.querySelector(".no5 p");
 daySmall.innerHTML = weekSmall[(day + 5) % 7];
 }
-/*convert unit
-function farToCel(event) {
-  event.preventDefault();
-  let temp = document.querySelector("#temp");
-  temp.innerHTML = "-6";
-}
-function CelToFar(event) {
-  event.preventDefault();
-  let temp = document.querySelector("#temp");
-  temp.innerHTML = "7";
-}
-let cel = document.querySelector("#cel");
-let far = document.querySelector("#far");
-cel.addEventListener("click", farToCel);
-far.addEventListener("click", CelToFar);*/
-//////API GEOLOCATION////
-let apiKey = "5c295f8a5977862a68c4b22eb14217b5";
-
 
 function getInfoOfCity(response){
 let cityName = document.querySelector(".location-name-big");
 cityName.innerHTML=response.data.name;
 let cityTemp= document.querySelector("#temp");
 cityTemp.innerHTML= Math.round(response.data.main.temp);
+celcius = Math.round(response.data.main.temp);
 let cityDescription =document.querySelector(".weather-name-big"); 
 cityDescription.innerHTML=response.data.weather[0].description;
 let humidity = document.querySelector(".weather-humidity");
 humidity.innerHTML=`<strong>humidity:</strong> ${response.data.main.humidity}%`;
 let wind = document.querySelector(".weather-wind");
 wind.innerHTML=`<strong>Wind:</strong> ${response.data.wind.speed}m/s`;
-let percipitation = document.querySelector(".precipitation");
-percipitation.innerHTML=`<strong>Feels like:</strong> ${Math.round(response.data.main.feels_like)}℃`;
+let percipitation = document.querySelector("#feels");
+percipitation.innerHTML=`${Math.round(response.data.main.feels_like)}`;
 lastUpdated(response.data.dt*1000);
 let image = document.querySelector(".weather-photo-big");
 image.setAttribute("src",`images/big/${response.data.weather[0].icon}.svg`);
@@ -99,7 +83,6 @@ function getCurrent(pos) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.longitude}&lon=${pos.coords.latitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(getInfoOfCity);
 }
-navigator.geolocation.getCurrentPosition(getCurrent);
 
 function getResponse(cityName){
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
@@ -114,15 +97,72 @@ input.value='';
 getResponse(input2);
 }
 
-let search = document.querySelector("#search");
-search.addEventListener("submit",changeLocationToValue);
-
 function prevent(event){
  event.preventDefault();
  navigator.geolocation.getCurrentPosition(getCurrent);
+
+
 }
 
+function toFarenheit(num){
+ return Math.round(num*9/5 +32);
+}
+
+function toCelcius(num){
+  return Math.round((num-32)*5 / 9);
+}
+
+function toFarenheitAll(event){
+event.preventDefault();
+let tempBig= document.querySelector("#temp");
+if(tempBig.innerHTML==celcius){
+  tempBig.innerHTML=toFarenheit(tempBig.innerHTML);
+  let feels = document.querySelector("#feels");
+  feels.innerHTML =toFarenheit(feels.innerHTML);
+  let signLittle = document.querySelector("#signLittle");
+ signLittle.innerHTML ="°F";
+ /* let no1 = document.querySelector(".no1");
+  no1.innerHTML=toFarenheit(no1.innerHTML);
+  let no2 =document.querySelector(".no2");
+  no2.innerHTML=toFarenheit(no2.innerHTML);
+  let no3 =document.querySelector(".no3");
+  no3.innerHTML=toFarenheit(no3.innerHTML);
+  let no4 =document.querySelector(".no4");
+  no4.innerHTML=toFarenheit(no4.innerHTML);
+  let no5 = document.querySelector(".no5");
+  no5.innerHTML=toFarenheit(no5.innerHTML);*/
+}
+else{
+  return;
+}
+}
+
+function toCelciusAll(event){
+  event.preventDefault();
+let tempBig= document.querySelector("#temp");
+if(tempBig.innerHTML==celcius){
+  return;
+}
+else{
+  tempBig.innerHTML=toCelcius(tempBig.innerHTML);
+  let feels = document.querySelector("#feels");
+  feels.innerHTML = toCelcius(feels.innerHTML);
+  let signLittle = document.querySelector("#signLittle");
+ signLittle.innerHTML ="°C";
+}
+}
+
+let celcius = null;
+navigator.geolocation.getCurrentPosition(getCurrent);
+let search = document.querySelector("#search");
+search.addEventListener("submit",changeLocationToValue);
 let here = document.querySelector("#here");
 here.addEventListener("click",prevent);
 systemDate();
-
+let far=document.querySelector("button#far");
+far.addEventListener("click",toFarenheitAll);
+let cel= document.querySelector("button#cel");
+cel.addEventListener("click",toCelciusAll);
+/*let cel=document.querySelector("button#cel");
+far.addEventListener("click",toCelciusAll);
+*/
